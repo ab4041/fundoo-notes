0f960156361b6ab732f859f5bac49e2cb7b19951
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-04-24 11:44:25 +0530

    UC1: project setup and package structure

---
+++ fundoonotes/src/main/java/com/fundoonotes/service/UserService.java
@@ -0,0 +1,10 @@
+package com.fundoonotes.service;
+
+import com.fundoonotes.dto.request.UserRegisterRequestDto;
+import com.fundoonotes.dto.response.UserResponseDto;
+
+public interface UserService {
+
+    UserResponseDto register(UserRegisterRequestDto dto);
+
+}
